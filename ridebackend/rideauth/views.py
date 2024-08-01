a4a2546d6212fc7bebe7a2e5a13b85adc859bf77
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-01 18:38:52 +0300

    RC-56 Add the login url path and return proper messages for credentials input from the login view

--- ridebackend/rideauth/views.py
+++ ridebackend/rideauth/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
 from .serializers import UserSerializer
+from .models import User
 # Create your views here.
 
 class RegisterView(APIView):
@@ -13,4 +15,16 @@ class RegisterView(APIView):
 
 class LoginView(APIView):
     def post (self,request):
-        pass
+        email = request.data['email']
+        password = request.data['password']
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed("User not found!")
+
+        if not user.check_password(password):
+            raise AuthenticationFailed("Incorrect password")
+        
+        return Response({
+            'message': 'success'
+        })
