7a73f8a1090139f676ec9cb73cd96a607042d553
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-01 17:24:00 +0300

    RC-56 Hash the password

--- ridebackend/rideauth/serializers.py
+++ ridebackend/rideauth/serializers.py
@@ -4,3 +4,11 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'name', 'email', 'password']
+
+    def create(self, validate_data):
+        password = validate_data.pop('password', None)
+        instance = self.Meta.model(**validate_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
--- ridebackend/rideauth/views.py
+++ ridebackend/rideauth/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializers import UserSerializer
 # Create your views here.
 
@@ -8,3 +9,4 @@ class RegisterView(APIView):
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        return Response(serializer.data)
