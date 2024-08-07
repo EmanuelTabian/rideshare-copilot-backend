851d22f3ce20193f1559f8c54bc87a91244d447d
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-07 18:08:23 +0300

    RC-59 Fix validated data typo on the user serializer

--- ridebackend/rideauth/serializers.py
+++ ridebackend/rideauth/serializers.py
@@ -8,10 +8,12 @@ class UserSerializer(serializers.ModelSerializer):
             'password': {'write_only': True}
         }
 
-    def create(self, validate_data):
-        password = validate_data.pop('password', None)
-        instance = self.Meta.model(**validate_data)
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
         if password is not None:
             instance.set_password(password)
         instance.save()
         return instance
+    
+   
