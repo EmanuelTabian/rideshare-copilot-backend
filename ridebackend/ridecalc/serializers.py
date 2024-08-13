49af8f9ada57fe58b7d242280cccd30d2afec0ff
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-13 22:08:19 +0300

    RC-66 Set up a calculator entry serializer and a create method

---
+++ ridebackend/ridecalc/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import CalculatorEntry
+class CalculatorEntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CalculatorEntry
+        fields = ['user', 'pub_date', 'app_income', 'commission', 'expenses']
+
+        def create(self, validated_data):
+            instance = self.Meta.model(**validated_data)
+            instance.save()
+            return instance
