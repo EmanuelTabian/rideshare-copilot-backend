3a48eed1fa8447f6ccaf34a6dadf3950d2959f0b
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-14 00:25:59 +0300

    RC-66 Add a new earnings field and alter epense max digitsvalue

--- ridebackend/ridecalc/models.py
+++ ridebackend/ridecalc/models.py
@@ -7,5 +7,5 @@ class CalculatorEntry(models.Model):
     pub_date = models.DateTimeField('Entry date', auto_now_add=True)
     app_income = models.DecimalField("App income" ,max_digits=5, decimal_places=2)
     commission = models.DecimalField("Commission" ,max_digits=5, decimal_places=2, null=True, blank=True)
-    expenses = models.DecimalField("Expenses" ,max_digits=5, decimal_places=2,null=True, blank=True)
-
+    expenses = models.DecimalField("Expenses" ,max_digits=10, decimal_places=2,null=True, blank=True)
+    earnings = models.DecimalField("Earnings" ,max_digits=10, decimal_places=2,null=True, blank=True)
--- ridebackend/ridecalc/views.py
+++ ridebackend/ridecalc/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import CalculatorEntrySerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class AddCalculatorEntry(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
         serializer = CalculatorEntrySerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
