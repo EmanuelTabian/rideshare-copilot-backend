f02c86e7c9f2b276838a794286ff0cdc8d34220d
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-13 21:39:15 +0300

    RC-66 set commission and expenses as optional fields

--- ridebackend/ridecalc/models.py
+++ ridebackend/ridecalc/models.py
@@ -6,6 +6,6 @@ class CalculatorEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField('Entry date', auto_now_add=True)
     app_income = models.DecimalField("App income" ,max_digits=5, decimal_places=2)
-    commission = models.DecimalField("Commission" ,max_digits=5, decimal_places=2)
-    expenses = models.DecimalField("Expenses" ,max_digits=5, decimal_places=2)
+    commission = models.DecimalField("Commission" ,max_digits=5, decimal_places=2, null=True, blank=True)
+    expenses = models.DecimalField("Expenses" ,max_digits=5, decimal_places=2,null=True, blank=True)
 
