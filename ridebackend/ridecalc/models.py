2264af20d197a4021c8c3488d88a89f8e227ce76
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-14 00:29:00 +0300

    RC-66 Set max digits value to 10 for all fields, and migrate modifications to the database

--- ridebackend/ridecalc/models.py
+++ ridebackend/ridecalc/models.py
@@ -5,7 +5,7 @@ from rideauth.models import User
 class CalculatorEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField('Entry date', auto_now_add=True)
-    app_income = models.DecimalField("App income" ,max_digits=5, decimal_places=2)
-    commission = models.DecimalField("Commission" ,max_digits=5, decimal_places=2, null=True, blank=True)
+    app_income = models.DecimalField("App income" ,max_digits=10, decimal_places=2)
+    commission = models.DecimalField("Commission" ,max_digits=10, decimal_places=2, null=True, blank=True)
     expenses = models.DecimalField("Expenses" ,max_digits=10, decimal_places=2,null=True, blank=True)
     earnings = models.DecimalField("Earnings" ,max_digits=10, decimal_places=2,null=True, blank=True)
