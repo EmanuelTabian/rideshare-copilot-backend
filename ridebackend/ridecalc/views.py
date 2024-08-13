de29e7967a65a13bea0c65cf56e73ff463fc45b2
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-13 22:23:31 +0300

    Migrate the calculator model and set up an add calc entry api endpoint

--- ridebackend/ridecalc/views.py
+++ ridebackend/ridecalc/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import CalculatorEntrySerializer
 
 # Create your views here.
+class AddCalculatorEntry(APIView):
+    def post(self, request):
+        serializer = CalculatorEntrySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
