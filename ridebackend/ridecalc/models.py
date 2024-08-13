78b24d64a161837d7fb5839d93e6bc3276b75936
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-13 21:32:40 +0300

    RC-66 Define a calculator entry model

--- ridebackend/ridecalc/models.py
+++ ridebackend/ridecalc/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from rideauth.models import User
 
 # Create your models here.
+class CalculatorEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('Entry date', auto_now_add=True)
+    app_income = models.DecimalField("App income" ,max_digits=5, decimal_places=2)
+    commission = models.DecimalField("Commission" ,max_digits=5, decimal_places=2)
+    expenses = models.DecimalField("Expenses" ,max_digits=5, decimal_places=2)
+
