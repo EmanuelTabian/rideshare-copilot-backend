bf8fb74fadddf41a88019011aa6275f2ce499123
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-14 01:41:35 +0300

    RC-66 Add null and blank attributes to the user field

--- ridebackend/ridecalc/models.py
+++ ridebackend/ridecalc/models.py
@@ -3,7 +3,7 @@ from rideauth.models import User
 
 # Create your models here.
 class CalculatorEntry(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     pub_date = models.DateTimeField('Entry date', auto_now_add=True)
     app_income = models.DecimalField("App income" ,max_digits=10, decimal_places=2)
     commission = models.DecimalField("Commission" ,max_digits=10, decimal_places=2, null=True, blank=True)
--- ridebackend/ridecalc/serializers.py
+++ ridebackend/ridecalc/serializers.py
@@ -3,7 +3,7 @@ from .models import CalculatorEntry
 class CalculatorEntrySerializer(serializers.ModelSerializer):
     class Meta:
         model = CalculatorEntry
-        fields = ['user', 'pub_date', 'app_income', 'commission', 'expenses', 'earnings']
+        fields = ['user','pub_date', 'app_income', 'commission', 'expenses', 'earnings']
 
         def create(self, validated_data):
             instance = self.Meta.model(**validated_data)
