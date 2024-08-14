c31773b37adeffaf22e80524764ba4bedf419c56
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-14 18:51:44 +0300

    RC-66 Define the delete calculator entry url path

--- ridebackend/ridecalc/urls.py
+++ ridebackend/ridecalc/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AddCalculatorEntry,GetCalculatorEntries,UpdateCalculatorEntries
+from .views import AddCalculatorEntry,GetCalculatorEntries,UpdateCalculatorEntries,DeleteCalculatorEntries
 
 urlpatterns = [
     path('add-calculator-entry', AddCalculatorEntry.as_view()),
     path('get-calculator-entries', GetCalculatorEntries.as_view()),
-    path('update-calculator-entry/<int:calcentry_id>', UpdateCalculatorEntries.as_view())
+    path('update-calculator-entry/<int:calcentry_id>', UpdateCalculatorEntries.as_view()),
+     path('delete-calculator-entry/<int:calcentry_id>', DeleteCalculatorEntries.as_view())
 ]
