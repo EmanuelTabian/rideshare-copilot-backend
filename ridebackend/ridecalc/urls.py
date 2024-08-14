6efc234b6ff7ee76270ec6758bb1a9d4493f77da
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-14 17:57:25 +0300

    Set up the calculator entry positional argument in the url path

--- ridebackend/ridecalc/urls.py
+++ ridebackend/ridecalc/urls.py
@@ -4,6 +4,5 @@ from .views import AddCalculatorEntry,GetCalculatorEntries,UpdateCalculatorEntri
 urlpatterns = [
     path('add-calculator-entry', AddCalculatorEntry.as_view()),
     path('get-calculator-entries', GetCalculatorEntries.as_view()),
-    path('update-calculator-entry', UpdateCalculatorEntries.as_view())
-
+    path('update-calculator-entry/<int:calcentry_id>', UpdateCalculatorEntries.as_view())
 ]
