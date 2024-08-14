115f25d35993678ab49b57de762875a82f989e50
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-14 17:37:49 +0300

    RC-66 Define a patch method serializer

--- ridebackend/ridecalc/serializers.py
+++ ridebackend/ridecalc/serializers.py
@@ -9,3 +9,9 @@ class CalculatorEntrySerializer(serializers.ModelSerializer):
             instance = self.Meta.model(**validated_data)
             instance.save()
             return instance
+        
+        def patch(self, instance, validated_data):
+            for attr, value in validated_data.items():
+                setattr(instance, attr, value)
+            instance.save()
+            return instance
