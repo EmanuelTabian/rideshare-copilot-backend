575b6fcb1da5c7ccfb193971c8afd4c5ae042739
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-14 17:39:04 +0300

    RC-66 Set a new url pattern for calculator entry update

--- ridebackend/ridecalc/urls.py
+++ ridebackend/ridecalc/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import AddCalculatorEntry,GetCalculatorEntries
+from .views import AddCalculatorEntry,GetCalculatorEntries,UpdateCalculatorEntries
 
 urlpatterns = [
     path('add-calculator-entry', AddCalculatorEntry.as_view()),
-    path('get-calculator-entries', GetCalculatorEntries.as_view())
+    path('get-calculator-entries', GetCalculatorEntries.as_view()),
+    path('update-calculator-entry', UpdateCalculatorEntries.as_view())
+
 ]
