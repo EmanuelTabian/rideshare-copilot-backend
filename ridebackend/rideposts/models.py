bc3b4830fd23bd6fadc634d4d4919c2673b66849
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-16 02:22:43 +0300

    RC-69 Define a file upload model

--- ridebackend/rideposts/models.py
+++ ridebackend/rideposts/models.py
@@ -1,17 +1,29 @@
 from django.db import models
+from django.conf import settings
 
-# def file_generate_upload_path(instance, filename):
-#     return f"files/{instance.file_name}"
 
-# class File(models.Model):
-#     file = models.FileField(
-#         upload_to=file_generate_upload_path,
-#         blank=True,
-#         null=True
-#     )
+def file_generate_upload_path(instance, filename):
+    return f"files/{instance.file_name}"
 
-#     original_file_name = models.TextField()
+class File(models.Model):
+    file = models.FileField(
+        upload_to=file_generate_upload_path,
+        blank=True,
+        null=True
+    )
 
-#     file_name = models.CharField(max_length=255, unique=True)
-#     file_type
+    original_file_name = models.TextField()
+
+    file_name = models.CharField(max_length=255, unique=True)
+    file_type = models.CharField(max_length=255)
+
+    upload_finished_at = models.DateTimeField(blank=True, null=True)
+
+    @property 
+    def is_valid(self):
+        return bool(self.upload_finished_at)
+    
+    @property
+    def url(self):
+        if settings.FILE_UPLOAD_STORAGE == "s3"
 
