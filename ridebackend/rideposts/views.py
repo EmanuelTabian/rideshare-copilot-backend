81beb637474ddef2e09d7eba32e885778bec9589
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-16 19:44:30 +0300

    Define a file direc upload start api view and perform additional configuration

--- ridebackend/rideposts/views.py
+++ ridebackend/rideposts/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 
 from django.shortcuts import get_object_or_404
 
-from rest_framework import serializers
+from serializers import InputSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import File
 
 
-
+class FileDirectUploadStartApi(APIView):
+    def post(self,request):
+        serializer = InputSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data)
+      
 
