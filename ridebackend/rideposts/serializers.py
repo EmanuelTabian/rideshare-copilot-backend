511e1a8b364ecb5c78e47019f3b4da9fbe146e0b
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-26 16:26:34 +0300

    RC-69 Modify the start upload api to feature presigned data and directly define the input serializer inside it

--- ridebackend/rideposts/serializers.py
+++ ridebackend/rideposts/serializers.py
@@ -1,7 +1,5 @@
 
 from rest_framework import serializers
 
-class InputSerializer(serializers.Serializer):
-    file_name = serializers.CharField()
-    file_type = serializers.CharField()
+
     
--- ridebackend/rideposts/views.py
+++ ridebackend/rideposts/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
-
 from django.shortcuts import get_object_or_404
-
-from serializers import InputSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import File
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import serializers
 
 
 class FileDirectUploadStartApi(APIView):
-    def post(self,request):
-        serializer = InputSerializer(data=request.data)
+    permission_classes = [IsAuthenticated]
+
+    class InputSerializer(serializers.Serializer):
+        file_name = serializers.CharField()
+        file_type = serializers.CharField()
+
+
+    def post(self,request, *args, **kwargs):
+        serializer = self.InputSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(data=serializer.data)
+        service = FileDirectUploadService(request.user)
+        presigned_data = service.start(**serializer.validated_data)
+
+        return Response(data=presigned_data.data)
       
 
