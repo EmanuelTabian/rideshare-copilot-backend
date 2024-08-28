d459261b6aa19fd52215710f6632a903812b8412
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-28 14:17:36 +0300

    RC-69 Build a method that generates the presigned post

--- ridebackend/rideposts/services.py
+++ ridebackend/rideposts/services.py
@@ -1,8 +1,23 @@
 from django.db import transaction
-from .models import File
+from .models import File, file_generate_upload_path
 from django.utils import timezone
+from django.conf import settings
 import pathlib
 from uuid import uuid4
+import boto3
+
+
+def s3_generate_presigned_post(*, file_path, file_type):
+    client = boto3.client(
+        service_name="s3",
+        aws_access_key_id=settings.AWS_S3_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_S3_SECRET_ACCESS_KEY,
+        region_name=settings.AWS_S3_REGION_NAME
+    )
+    presigned_data = client.generate_presigned_post(settings.AWS_STORAGE_BUCKET_NAME, file_path, Fields={
+        "Content-Type": file_type 
+    }, Conditions=[{"Content-Type": file_type}], ExpiresIn=settings.AWS_QUERYSTRING_EXPIRE)
+    return presigned_data
 
 def file_generate_name(original_file_name):
     mime_type = pathlib.Path(original_file_name).suffix
