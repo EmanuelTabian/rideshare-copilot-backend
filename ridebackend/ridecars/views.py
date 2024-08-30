f633e68cfbc7eab5d3d8d08cb2cd1b87a050db32
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-30 15:13:47 +0300

    RC-79 Build an api view that handles cars detail post

--- ridebackend/ridecars/views.py
+++ ridebackend/ridecars/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import CarPostSerializer
 
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated
+class AddRidePost(APIView):
+    permissions = [IsAuthenticated]
+
+    def post(self,request): 
+        serializer = CarPostSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
