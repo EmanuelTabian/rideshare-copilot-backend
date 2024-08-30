a13dab0c35ef4091f31067ca9d4a6f012f4be54f
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-30 16:38:31 +0300

    RC-79 Write an API endpoint that updates a car post based on its id

--- ridebackend/ridecars/views.py
+++ ridebackend/ridecars/views.py
@@ -39,3 +39,16 @@ class GetUserCarPosts(APIView):
       serializer = CarPostSerializer(user_car_posts,many=True)
       return Response(serializer.data)
 
+class UpdateRidePost(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def patch(self, request, car_post_id):
+        car_post = get_object_or_404(CarPost, pk=car_post_id)
+
+        if car_post.user != request.user:
+            return Response({"detail": "You do not have permission to edit this car post."})
+        
+        serializer = CarPostSerializer(car_post, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
