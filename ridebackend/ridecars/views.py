a477e674a30ffed7e54bfc9caf9ca00952cdd16c
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-30 16:22:01 +0300

    RC-79 Define an API Endopoint that gets all the ride posts

--- ridebackend/ridecars/views.py
+++ ridebackend/ridecars/views.py
@@ -4,11 +4,21 @@ from rest_framework.response import Response
 from .serializers import CarPostSerializer
 
 from rest_framework.permissions import IsAuthenticated
+from .models import CarPost
 class AddRidePost(APIView):
-    permissions = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def post(self,request): 
         serializer = CarPostSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+class GetAllRidePosts(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        car_posts = CarPost.objects.all()
+        serializer = CarPostSerializer(car_posts, many=True)
+        return Response(serializer.data) 
+
