35f27897ceb21e9bd3b94c97ae59d574d93c377d
EmanuelTabian <emanuel.forwork@gmail.com>
2024-08-30 16:28:53 +0300

    RC-79 Write a get method that will get a car post based on a postional id argument

--- ridebackend/ridecars/views.py
+++ ridebackend/ridecars/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import CarPostSerializer
@@ -22,3 +22,10 @@ class GetAllRidePosts(APIView):
         serializer = CarPostSerializer(car_posts, many=True)
         return Response(serializer.data) 
 
+class GetRidePost(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, car_post_id):
+        car_post = get_object_or_404(CarPost, pk=car_post_id)
+        serializer = CarPostSerializer(car_post)
+        return Response(serializer.data)
