095abd3e5ce86e4ad6f6da6bdfccd416d63217ff
EmanuelTabian <emanuel.forwork@gmail.com>
2024-09-05 15:39:13 +0300

    RC-81 Add corresponding url path for file put action

--- ridebackend/rideposts/urls.py
+++ ridebackend/rideposts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import FileDirectUploadStartApi, FileDirectUploadFinishApi,GetImageByKey, DeleteImageByKey
+from .views import FileDirectUploadStartApi, FileDirectUploadFinishApi,GetImageByKey, DeleteImageByKey, EditImageByKey
 
 urlpatterns = [
     path('upload/direct/start', FileDirectUploadStartApi.as_view()),
     path('upload/direct/finish', FileDirectUploadFinishApi.as_view()),
     path('get-image-by-file-key/<path:file_key>', GetImageByKey.as_view()),
     path('delete-image-by-file-key/<path:file_key>', DeleteImageByKey.as_view()),
+    path('put-image-by-file-key/<path:file_key>', EditImageByKey.as_view()),
 
 ]
