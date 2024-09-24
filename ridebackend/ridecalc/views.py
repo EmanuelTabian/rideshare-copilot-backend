824c1b948f030b95a6dea08d22571f5b4f407186
EmanuelTabian <emanuel.forwork@gmail.com>
2024-09-24 19:29:10 +0300

    Set up a new url and experiment with query params usage

--- ridebackend/ridecalc/views.py
+++ ridebackend/ridecalc/views.py
@@ -29,13 +29,16 @@ class GetCalculatorEntries(APIView):
         serializer = CalculatorEntrySerializer(calcentries, many=True)
         return Response(serializer.data)
 
-class GetRecentCalcultaorEntries(APIView):
+class GetRecentCalculatorEntries(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        days =  request.data.get('days') # Default to 7 days if not provided
-        if days not in [7, 30, 90]:
+        days = request.query_params.get('days', '7')
+        if days is None or not days.isdigit() or int(days) not in [ 7, 30, 90]:
             return Response({"detail": "Invalid number of days. Must be 7, 30, or 90."}, status=400)
+
+        days = int(days) 
+        print(days)
 
         time_threshold = timezone.now() - timedelta(days=days)
         calcentries = CalculatorEntry.objects.all().filter(user=request.user, pub_date__gte=time_threshold)
