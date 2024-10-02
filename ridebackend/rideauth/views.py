cfd9d1344775c1b5f36546b06a73a32201e03980
EmanuelTabian <emanuel.forwork@gmail.com>
2024-10-02 19:47:20 +0300

    Revert set cookie settings

--- ridebackend/rideauth/views.py
+++ ridebackend/rideauth/views.py
@@ -54,7 +54,7 @@ class LoginView(APIView):
         token = jwt.encode(payload, os.getenv("TOKEN_SECRET"), algorithm="HS256")
         
         response = Response()
-        response.set_cookie(key="jwt", value=token, samesite='None',domain=None ,httponly=True)
+        response.set_cookie(key="jwt", value=token,httponly=True)
         response.data = {"jwt": token}
         return response
 
