667f98e4f7e77197c2b2bb5cd6e0177cee31cf88
EmanuelTabian <emanuel.forwork@gmail.com>
2024-10-04 15:58:18 +0300

    RC-95 Add an user admin class and experiment with user-admin console interaction

--- ridebackend/rideauth/admin.py
+++ ridebackend/rideauth/admin.py
@@ -1,1 +1,17 @@
+from django.contrib import admin
+from django import forms
+from .models import User
 
+class UserChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('name','email', 'username')
+
+class UserAdmin(admin.ModelAdmin):
+    form = UserChangeForm
+    list_display = ("id", "name", "email", "username")
+    list_display_links = ("id", "name")
+    search_fields = ("name", "email", "username")
+    list_per_page = 25
+
+admin.site.register(User, UserAdmin)
