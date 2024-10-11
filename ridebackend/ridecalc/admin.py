f2e3e3d8934e65907147d961f9709bfa4504f0c8
EmanuelTabian <emanuel.forwork@gmail.com>
2024-10-11 05:20:07 +0300

    Add calculator entries access for admin console

--- ridebackend/ridecalc/admin.py
+++ ridebackend/ridecalc/admin.py
@@ -1,1 +1,20 @@
-# Register your models here.
+from .models import CalculatorEntry
+from django import forms
+
+from django.contrib import admin
+
+class UserChangeForm(forms.ModelForm):
+
+    class Meta:
+        model = CalculatorEntry
+        fields = ("user", "app_income",'commission', 'expenses', 'earnings')
+
+  
+class CalculatorAdmin(admin.ModelAdmin):
+    form = UserChangeForm
+    list_display = ("id","app_income", "commission", "expenses", "earnings", "pub_date")
+    list_display_links = ("id", "app_income")
+    search_fields = ("app_income", "pub_date", "commission")
+    list_per_page = 25
+
+admin.site.register(CalculatorEntry, CalculatorAdmin)
