3eed46d044ee4885681fbcdf2e4705eb53694e08
EmanuelTabian <emanuel.forwork@gmail.com>
2024-10-15 04:02:00 +0300

    Hide the project secret key

--- ridebackend/ridebackend/settings.py
+++ ridebackend/ridebackend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-iuwp5zbhxz&gdt^5n##(o=@2-)gl#hv93_+xobj98p=n2=8gvv"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
